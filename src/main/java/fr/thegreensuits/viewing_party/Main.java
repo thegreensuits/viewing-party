6836317096b65ebcac3c3d2d5b7386fdbe998b06
jerembdn <work@jeremybdn.fr>
2024-01-17 23:18:52 +0100

    ✨ add spawn command

--- src/main/java/fr/thegreensuits/viewing_party/Main.java
+++ src/main/java/fr/thegreensuits/viewing_party/Main.java
@@ -2,6 +2,7 @@ package fr.thegreensuits.viewing_party;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.thegreensuits.viewing_party.command.CommandSpawn;
 import fr.thegreensuits.viewing_party.listener.AsyncPlayerChatListener;
 import fr.thegreensuits.viewing_party.listener.EntityDamageListener;
 import fr.thegreensuits.viewing_party.listener.PlayerInteractAtEntityListener;
@@ -19,11 +20,15 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         super.onEnable();
 
+        // - Register listeners
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), INSTANCE);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(), INSTANCE);
         getServer().getPluginManager().registerEvents(new AsyncPlayerChatListener(), INSTANCE);
         getServer().getPluginManager().registerEvents(new EntityDamageListener(), INSTANCE);
         getServer().getPluginManager().registerEvents(new PlayerInteractAtEntityListener(), INSTANCE);
+
+        // - Register commands
+        getCommand("spawn").setExecutor(new CommandSpawn());
 
         getLogger().info("ViewingParty plugin enabled");
     }
---
+++ src/main/java/fr/thegreensuits/viewing_party/command/CommandSpawn.java
@@ -0,0 +1,21 @@
+package fr.thegreensuits.viewing_party.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandSpawn implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)) return false;
+
+        Player player = (Player) sender;
+
+        player.teleport(player.getWorld().getSpawnLocation());
+        player.sendMessage("§5Vous avez été téléporté au point d'apparition.");
+
+        return true;
+	}
+}
